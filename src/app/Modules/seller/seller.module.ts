58e024711dcc6d6e399026e2491c7cb9c47db216
BilalAhmedMohammed <bilalelmaestro2019@gmail.com>
2022-03-04 18:00:20 +0200

    Update Structure Folders

---
+++ src/app/Modules/seller/seller.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SellerRoutingModule } from './seller-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SellerRoutingModule
+  ]
+})
+export class SellerModule { }
