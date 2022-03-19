bd87d3a56de9e9fb0a1825f146ef58d277817258
BilalAhmedMohammed <bilalelmaestro2019@gmail.com>
2022-03-19 13:05:10 +0200

    handel 404 page not found and logout

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,9 @@
+import { LoginService } from 'src/app/services/login/login.service';
 import { Component, OnInit } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -7,13 +11,20 @@ import { Output, EventEmitter } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   @Output() newItemEvent = new EventEmitter<boolean>();
-  constructor() { }
+  constructor(private cookieService: CookieService,private serviceLogin:LoginService) { }
   status: boolean = true;
   clickEvent(){
     this.newItemEvent.emit(this.status);
-    this.status = !this.status;      
+    this.status = !this.status;   
 }
   ngOnInit(): void {
+  }
+  handelLogout(){
+    this.serviceLogin.adminLogout().subscribe((res=>{
+      console.log('Logout Subscribee');
+      this.cookieService.set('token', '', { expires: new Date(new Date().getTime() -  1000 * 60 * 60 * 24) });
+      window.location.reload();
+    }));
   }
 
 }
