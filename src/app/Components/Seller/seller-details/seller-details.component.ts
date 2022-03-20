84dad393591feb3cd2bffda7cbdf99a14a7455f1
tsnm-x <tasneemsm1234@gmail.com>
2022-03-20 14:18:52 +0200

    changed seller details page

--- src/app/Components/Seller/seller-details/seller-details.component.ts
+++ src/app/Components/Seller/seller-details/seller-details.component.ts
@@ -12,6 +12,9 @@ export class SellerDetailsComponent implements OnInit, DoCheck {
   completedOrders : any;
   inProgressOrders : any;
   cancelledOrders : any;
+  cancelPercent : number = 0;
+  inProgressPercent : number = 0;
+  completedPercent : number = 0;
 
   constructor() { }
 
@@ -25,6 +28,9 @@ export class SellerDetailsComponent implements OnInit, DoCheck {
     this.completedOrders = this.orders.filter((item: any) => item.status === "delivered");
     this.inProgressOrders = this.orders.filter((item: any) => item.status === "in progress");
     this.cancelledOrders = this.orders.filter((item: any)=> item.status === "canceled");
+    this.completedPercent = Math.floor((this.completedOrders.length * 100) / this.orders.length) || 0;
+    this.inProgressPercent = Math.floor((this.inProgressOrders.length * 100) / this.orders.length) || 0;
+    this.cancelPercent = Math.floor((this.cancelledOrders.length * 100) / this.orders.length) || 0;
 
   }
 
